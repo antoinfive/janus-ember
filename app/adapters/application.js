72ad68448efb8307ec09a78ba7bd75d992d135a0
antoinfive <antoincampbell@live.com>
2016-02-22 17:53:48 -0500

    another adapter change

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,9 @@
 import ActiveModelAdapter from 'active-model-adapter';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-
+import ENV from '../config/environment';
 
 export default ActiveModelAdapter.extend(DataAdapterMixin, {
   namespace:"api/v1",
-  host: window.JanusEmberENV.APP.API_HOST,
+  host: ENV.APP.API_HOST,
   authorizer: "authorizer:devise"
 });
