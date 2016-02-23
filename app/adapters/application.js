b0e5307110b571a193b69812c9310dd388c660c6
antoinfive <antoincampbell@live.com>
2016-02-23 00:57:07 -0500

    made changes to test chrome extension

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,10 @@
 import ActiveModelAdapter from 'active-model-adapter';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-import ENV from '../config/environment';
+// import ENV from '../config/environment';
 
 export default ActiveModelAdapter.extend(DataAdapterMixin, {
   namespace:"api/v1",
-  host: ENV.APP.API_HOST,
+  // host: ENV.APP.API_HOST,
+  host: "http://localhost:3000",
   authorizer: "authorizer:devise"
 });
--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -1,6 +1,7 @@
 import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
-import ENV from '../config/environment';
+// import ENV from '../config/environment';
 
 export default DeviseAuthenticator.extend({
-  serverTokenEndpoint: ENV.APP.TOKEN_ENDPOINT
+  // serverTokenEndpoint: ENV.APP.TOKEN_ENDPOINT
+  serverTokenEndpoint: "http://localhost:3000/users/sign_in"
 });
