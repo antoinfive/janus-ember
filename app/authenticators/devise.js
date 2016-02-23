e56ebb308b3d4929d874e1b70b3c8fcba4b88418
antoinfive <antoincampbell@live.com>
2016-02-23 12:16:06 -0500

    sweetness for chrome-extension

--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -5,3 +5,7 @@ export default DeviseAuthenticator.extend({
   // serverTokenEndpoint: ENV.APP.TOKEN_ENDPOINT
   serverTokenEndpoint: "http://localhost:3000/users/sign_in"
 });
+
+// {"authenticated":{"authenticator":"authenticator:devise","token":"Aw2ZFF2KlMIopT_p1OSu","email":"adam@may.com"}}
+//
+// .match(/(?:"token":")(\w+)(?:","email")/)[1]
